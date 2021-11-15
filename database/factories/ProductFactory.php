de4d47765691c031a3f20f81b1dc54c8c28e6f88
XojiakbarA <akromov.x.1995@gmail.com>
2021-11-15 12:45:02 +0500

    created and filled products table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'price' => $this->faker->numberBetween(30, 1200),
+            'avail' => $this->faker->numberBetween(0, 1),
+            'category_id' => $this->faker->numberBetween(7, 26),
+            'brand_id' => $this->faker->numberBetween(1, 8)
+        ];
+    }
+}

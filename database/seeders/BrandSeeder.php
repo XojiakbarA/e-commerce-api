baa16021f6a05028c8e14bb838fe529ae83f5564
XojiakbarA <akromov.x.1995@gmail.com>
2021-11-15 12:28:50 +0500

    created and filled brands table

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i < 9; $i++) :
+            DB::table('brands')->insert([
+                'title' => 'Brand ' . $i
+            ]);
+        endfor;
+    }
+}

7413d85ccd69877ed1829def4d87aec34e22b8aa
XojiakbarA <akromov.x.1995@gmail.com>
2022-01-04 12:07:37 +0500

    installed intervention package

--- config/app.php
+++ config/app.php
@@ -175,9 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Darryldecode\Cart\CartServiceProvider::class,
-        App\Providers\WishlistProvider::class
-
-
+        App\Providers\WishlistProvider::class,
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -232,9 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Cart' => Darryldecode\Cart\Facades\CartFacade::class
-
-
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];

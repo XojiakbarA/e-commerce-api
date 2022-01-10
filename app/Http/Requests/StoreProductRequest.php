b4fc946215614a995cf3e45cef16fc2ce3c56683
XojiakbarA <akromov.x.1995@gmail.com>
2022-01-11 00:02:38 +0500

    created validation for creating a product

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string',
+            'category_id' => 'required|integer',
+            'sub_category_id' => 'required|integer',
+            'brand_id' => 'required|integer',
+            'description' => 'required|string',
+            'stock' => 'required|integer',
+            'price' => 'required|integer',
+            'sale_price' => 'nullable|integer',
+            'images.*' => 'nullable|image',
+            'shop_id' => 'required|integer'
+        ];
+    }
+}
